c3a536eda466c9a5dbc6708004ea7c2d8f34b011
Mateusz Kozakiewicz <mateusz.kozakiewicz@skygate.io>
2020-04-05 00:35:24 +0200

    fix folder name

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,2 @@
-rsync -avz --quiet --exclude 'deploy' --exclude '.travis.yml' $TRAVIS_BUILD_DIR/ build@$BITWARDEN_HOST:/srv/bitwarden
+rsync -avz --quiet --exclude 'scripts' --exclude '.travis.yml' $TRAVIS_BUILD_DIR/ build@$BITWARDEN_HOST:/srv/bitwarden
 ssh build@$BITWARDEN_HOST 'cd /srv/bitwarden; ./backup-db.sh; docker-compose up -d'
